9899b4e1cd626eea2d9f8d8a843c3b120fc58bb1
Artooorcheck <Artooorcheck@gmail.com>
2025-10-04 14:55:57 +0300

    Initial Commit

---
+++ ReaderLab/ReaderLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderLabApp.swift
+//  ReaderLab
+//
+//  Created by artur on 04.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ReaderLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
